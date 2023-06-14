7ae8cc52001145f06d6d51e2ebe4bd900459ec1c
OkkashaAlly <steamnature006@gmail.com>
2023-06-14 07:24:50 +0300

    APP/COMPONENTS/LAYOUTS: Header.tsx - added

---
+++ app/components/index.ts
@@ -0,0 +1,4 @@
+// LAYOUTS ========================================
+export { default as Header } from "./layouts/Header";
+// SHARED =========================================
+export { default as SearchBar } from "./shared/SearchBar";
---
+++ app/components/layouts/Header.tsx
@@ -0,0 +1,30 @@
+// COMPONENTS =========================================
+import { SearchBar } from "..";
+
+// ===================================================
+// HEADER LAYOUT COMPONENT (app/page.tsx) ============
+// ===================================================
+export default function Header() {
+  // RETURN ==========================================
+  return (
+    <header className="border-b border-b-gray-300 shadow-sm bg-white px-16 py-3">
+      <div className="container w-[78%] flex items-center justify-between">
+        <div className="flex gap-4 items-center">
+          {/* logo */}
+          <h1 className="text-2xl font-bold">News App</h1>
+          {/* search bar  */}
+          <SearchBar />
+        </div>
+        {/* buttons  */}
+        <div className="flex gap-4">
+          <button className="text-gray-600 px-4 py-2 rounded-md">Login</button>
+          <button className="border border-blue-500 text-blue-500 px-4 py-2 rounded-md">
+            Create account
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+// EXTENDED COMPONENTS =================================
